3560c516376295a4aff9aa9bd16a412f34ec3811
paolo <paolo_delpiano@hotmail.com>
2025-05-14 09:49:51 -0300

    Preparando despliegue en Railway

--- app/settings.py
+++ app/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -14,7 +15,8 @@ SECRET_KEY = 'django-insecure-28cd!us=2#$u#o09brr-^w98$7o&0-0)@4bop_yru*#nt@)t4%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
