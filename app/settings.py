f845c25c13b7a2719bcbaaaf6f25bb7287f6e3df
paolo <paolo_delpiano@hotmail.com>
2025-05-14 10:15:22 -0300

    Preparando despliegue en Railway

--- app/settings.py
+++ app/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-28cd!us=2#$u#o09brr-^w98$7o&0-0)@4bop_yru*#nt@)t4%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ["*"]
