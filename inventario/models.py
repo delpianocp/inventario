0e22f56987427f827de123f518ee232ae167b24a
paolo <paolo_delpiano@hotmail.com>
2025-05-17 16:11:03 -0300

    modifico select editar articuloen plantillas

--- inventario/models.py
+++ inventario/models.py
@@ -8,6 +8,7 @@ class Articulo(models.Model):
     descripcion = models.TextField()
     cantidad = models.PositiveIntegerField()
     asignacion = models.CharField(max_length=255)
+    categoria = models.CharField(max_length=255)
     valor = models.DecimalField(max_digits=10, decimal_places=2)
     foto = CloudinaryField('image', blank=True, null=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)  # Relación con el usuario que lo creó
