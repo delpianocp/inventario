1daa457173858a7bf489fd1fe26b6c519c39facb
paolo <paolo_delpiano@hotmail.com>
2025-05-17 14:22:37 -0300

    agrega categoria correccion andando mal

--- inventario/models.py
+++ inventario/models.py
@@ -2,24 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-class Categoria(models.Model):
-    nombre = models.CharField(max_length=100, unique=True)
-
-    def __str__(self):
-        return self.nombre
-
-
 class Articulo(models.Model):
     nombre = models.CharField(max_length=255)
     descripcion = models.TextField()
     cantidad = models.PositiveIntegerField()
     asignacion = models.CharField(max_length=255)
+    categoria = models.CharField(max_length=255)
     valor = models.DecimalField(max_digits=10, decimal_places=2)
     foto = CloudinaryField('image', blank=True, null=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)  # Relación con el usuario que lo creó
-    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, related_name="articulos")
-
-
 
     def __str__(self):
         return f"{self.nombre} - {self.usuario.username}"
