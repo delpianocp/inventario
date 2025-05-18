d8e366e7fc93c058277c6fd489a3bcedfe847a60
paolo <paolo_delpiano@hotmail.com>
2025-05-17 22:55:17 -0300

    cierre de sesion por inactividad

--- app/settings.py
+++ app/settings.py
@@ -156,6 +156,8 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 CLOUDINARY_URL = 'CLOUDINARY_URL=cloudinary://<464665576365125>:<Vayt6oR9UDicyFKCwKUi-Vz5j3g>@dxra3bnrm'
 
-
+SESSION_COOKIE_AGE = 60  # 1800 segundos = 30 minutos
+SESSION_EXPIRE_AT_BROWSER_CLOSE = False  # La sesión sigue abierta si el usuario no cierra el navegador
+SESSION_SAVE_EVERY_REQUEST = True  # Reinicia el contador de inactividad con cada petición
 
 
